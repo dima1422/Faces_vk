cdd218564b1816a26761390a7717595921e0c0c4
dima1422 <plastund@gmail.com>
2017-09-24 16:32:47 +0300

    ver3

--- Utils.py
+++ Utils.py
@@ -90,8 +90,10 @@ def get_image_descriptor(image):
     sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
     facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
     detector = dlib.get_frontal_face_detector()
-
-    img= io.imread(image)
+    try:
+        img= io.imread(image)
+    except Exception:
+        print('ошибка')
     # print(img.shape)
     # img = rgb2gray(img)
 
